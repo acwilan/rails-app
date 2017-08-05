b3639a8477961ce01e79431ed9e01ed1efee767f
Andres Rov <andres.rovira@gmail.com>
2017-08-05 02:24:39 +0000

    Agregar modelo de usuarios

--- db/migrate/20170805015627_create_users.rb
+++ db/migrate/20170805015627_create_users.rb
@@ -3,8 +3,6 @@ class CreateUsers < ActiveRecord::Migration[5.1]
     create_table :users do |t|
       t.string :name
       t.string :email
-      t.string :password_digest
-      t.boolean :active
 
       t.timestamps
     end
