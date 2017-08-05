abf06859e5d547b1568ab2fdbc0f1c4bf07c721f
Andres Rov <andres.rovira@gmail.com>
2017-08-05 01:47:49 +0000

    Terminando layout base

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+  get 'users/new'
+
   get 'static_pages/home'
 
   get 'static_pages/help'
@@ -61,8 +63,9 @@ Rails.application.routes.draw do
   #   end
   # resources :users
   # resources :microposts
-  get  'static_pages/home'
-  get  'static_pages/help'
-  get  'static_pages/about'
   root 'static_pages#home'
+  get  '/help', to: 'static_pages#help'
+  get  '/about', to: 'static_pages#about'
+  get  '/contact', to: 'static_pages#contact'
+  get  '/signup',  to: 'users#new'
 end
